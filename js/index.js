be7decf7c13724ff1685cf6608ce4f0e3c5dcbf0
christian <christiancasey969@gmail.com>
2017-06-01 17:20:46 +0100

    refactoring navbar to be responsive

--- js/index.js
+++ js/index.js
@@ -182,3 +182,13 @@ function imageScroll(c){
 $(document).ready(function() {
 	$('.centered').fadeIn(2000);
 });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+} 
